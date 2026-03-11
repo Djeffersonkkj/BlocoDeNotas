2ecba63f36cd94080d5e91eb51a4292f2b88ef04
Djefferson <djefferson141005@gmail.com>
2026-03-11 18:35:50 -0300

    feat: subindo projeto bacana

---
+++ BlocoDeNotas/Program.cs
@@ -0,0 +1,16 @@
+﻿using BlocoDeNotas.Models;
+using BlocoDeNotas.Repository;
+
+UsuarioRepository usuarioRepository = new UsuarioRepository();
+NotaRepository notaRepository = new NotaRepository();
+
+var usuario = usuarioRepository.ChamarUsuario("djefferson141005@gmail.com", "123456789");
+System.Console.WriteLine(usuario.ToString());
+
+
+var notas = notaRepository.ChamarNotas(usuario);
+System.Console.WriteLine();
+foreach ( var nota in notas)
+{
+    System.Console.WriteLine(nota.ExibirNota());
+}
